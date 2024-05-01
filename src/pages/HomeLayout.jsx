7273870370f34665a5a8432233d71c5ed173ea83
virghiu <virghiu@gmail.com>
2024-04-30 20:04:16 -0400

    create header components

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
 
 const HomeLayout = () => {
   return (
     <>
+      <Header />
       <nav>
         <span className='text-4xl text-primary'>Comfy</span>
       </nav>
