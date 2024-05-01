ec2f1b776c8ab91dab99a8eaed4413a522ef0822
virghiu <virghiu@gmail.com>
2024-04-30 22:02:01 -0400

    add theme handler winter-dracula

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs'
 import { FaBarsStaggered } from 'react-icons/fa6'
 import NavLinks from './NavLinks'
 
+const themes = {
+  winter: 'winter',
+  dracula: 'dracula',
+}
+
+const getThemeFromLocalStorage = () =>
+  localStorage.getItem('theme') || themes.winter
+
 const Navbar = () => {
-  const [theme, setTheme] = useState(false)
+  const [theme, setTheme] = useState(getThemeFromLocalStorage())
   const handleTheme = () => {
-    setTheme(!theme)
+    const { winter, dracula } = themes
+    const newTheme = theme === winter ? dracula : winter
+    setTheme(newTheme)
   }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
+  }, [theme])
 
   return (
     <nav className='bg-base-200'>
