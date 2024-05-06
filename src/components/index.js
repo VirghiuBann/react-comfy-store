bb663eb87e99c93b76ca0bf1f2c809cbe0ff7973
virghiu <virghiu@gmail.com>
2024-05-06 15:06:28 -0400

    create error element and add to App.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as FormInput } from './FormInput'
 export { default as SubmitBtn } from './SubmitBtn'
 export { default as Hero } from './Hero'
+export { default as ErrorElement } from './ErrorElement'
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,14 @@
 import { Hero } from '../components'
+import { customFetch } from '../utils'
+
+const url = '/products?featured=true'
+
+export const loader = async () => {
+  const response = await customFetch(url)
+  console.log(response)
+  const product = response.data.data
+  return { product }
+}
 
 const Landing = () => {
   return (
