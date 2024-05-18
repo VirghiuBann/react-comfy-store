b3c10aeea06bd3bbb5b01d531539d6f381a29beb
virghiu <virghiu@gmail.com>
2024-05-17 23:30:27 -0400

    add action demo

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,15 @@ import {
 } from './pages'
 import { ErrorElement } from './components'
 
+//loaders
 import { loader as landingLoader } from './pages/Landing'
 import { loader as singleProductLoader } from './pages/SingleProduct'
 import { loader as productsLoader } from './pages/Products'
+import { loader as checkoutLoader } from './pages/Checkout'
+// actions
 import { action as registerAction } from './pages/Register'
 import { action as loginAction } from './pages/Login'
-import { loader as checkoutLoader } from './pages/Checkout'
+import { action as checkoutAction } from './components/CheckoutForm'
 
 import { store } from './store'
 
@@ -56,6 +59,7 @@ const router = createBrowserRouter([
         path: 'checkout',
         element: <Checkout />,
         loader: checkoutLoader(store),
+        action: checkoutAction(store),
       },
       {
         path: 'orders',
--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,5 +1,23 @@
+import { Form } from 'react-router-dom'
+import FormInput from './FormInput'
+import SubmitBtn from './SubmitBtn'
+
+export const action = (store) => async () => {
+  console.log(store)
+  return null
+}
+
 const CheckoutForm = () => {
-  return <div>CheckoutForm</div>
+  return (
+    <Form method='post' className='flex flex-col gap-y-4'>
+      <h4 className='font-medium text-xl'>shipping information</h4>
+      <FormInput label='first name' name='name' type='text' />
+      <FormInput label='address' name='address' type='text' />
+      <div className='mt-4'>
+        <SubmitBtn text='place your order' />
+      </div>
+    </Form>
+  )
 }
 
 export default CheckoutForm
