cf312088d8b14731cede2e02ef32fd74a9896219
virghiu <virghiu@gmail.com>
2024-05-18 10:12:23 -0400

    create orders loader

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,3 +1,47 @@
+import { redirect } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { customFetch } from '../utils'
+
+export const loader =
+  (store) =>
+  async ({ request }) => {
+    const user = store.getState().userState.user
+
+    if (!user) {
+      toast.warn('You must be logged in the view orders')
+      return redirect('/login')
+    }
+
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ])
+
+    try {
+      const resp = await customFetch.get('/orders', {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      })
+
+      return {
+        orders: resp.data.data,
+        meta: resp.data.meta,
+      }
+    } catch (error) {
+      console.log(error)
+      const errorMsg =
+        error?.response?.data?.error?.message ||
+        'there was an error accessing your orders'
+
+      toast.error(errorMsg)
+
+      if (error?.response?.status === 401 || error?.response?.status === 403)
+        return redirect('/login')
+
+      return null
+    }
+  }
 const Orders = () => {
   return <div>Orders</div>
 }
