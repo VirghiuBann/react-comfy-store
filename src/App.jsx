cac2aea326d0fe7183af6933657bfa7eacf75665
virghiu <virghiu@gmail.com>
2024-05-20 16:57:46 -0400

    setup react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import {
   HomeLayout,
@@ -27,6 +29,14 @@ import { action as loginAction } from './pages/Login'
 import { action as checkoutAction } from './components/CheckoutForm'
 
 import { store } from './store'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
 
 const router = createBrowserRouter([
   {
@@ -88,7 +98,12 @@ const router = createBrowserRouter([
 ])
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
