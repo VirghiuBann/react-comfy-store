2f8428b81b0c8b277c53adf6fdeb5a14189413fe
virghiu <virghiu@gmail.com>
2024-05-20 18:40:07 -0400

    setup react query get all products

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ const router = createBrowserRouter([
       {
         path: 'products',
         element: <Products />,
-        loader: productsLoader,
+        loader: productsLoader(queryClient),
         errorElement: ErrorElement,
       },
       {
